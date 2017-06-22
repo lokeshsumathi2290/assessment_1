f26e405069a443523cae8d5ef23c75c0ae839571
root <root@ip-172-31-17-231.us-west-2.compute.internal>
2017-06-22 17:27:50 +0000

    Updating Startup Script

--- startup_script/startup.sh
+++ startup_script/startup.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
+###To avoid dos issue
+cat file_name.sh | tr -d '\r' > file_name.sh.new
 
+cat ../database/conf/config_mysql.sh | tr -d '\r' > ../database/conf/config_mysql.sh.new
+cat ../database/conf/start.sh | tr -d '\r' > ../database/conf/start.sh.new
+cat ../webserver/conf/start.sh | tr -d '\r' > ../webserver/conf/start.sh.new
+
+rm -f ../database/conf/config_mysql.sh ../database/conf/start.sh ../webserver/conf/start.sh
+
+mv ../database/conf/config_mysql.sh.new ../database/conf/config_mysql.sh
+mv ../database/conf/start.sh.new ../database/conf/start.sh
+mv ../webserver/conf/start.sh.new ../webserver/conf/start.sh
+
+
+###Docker Build and Run Commands
 database_build="database:0.1"
 middleware_build="middleware:0.1"
 webserver_build="webserver:0.1"
